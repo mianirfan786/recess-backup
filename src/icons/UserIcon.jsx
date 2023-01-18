90a9d672f0063ca65b4cbd4c30b6a264928d8425
dharmasarswat <dharmasarswat001@gmail.com>
2023-01-18 17:25:06 +0530

    - added icons for common use

---
+++ src/icons/UserIcon.jsx
@@ -0,0 +1,29 @@
+const UserIcon = () => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <ellipse
+      cx="10.0001"
+      cy="14.5833"
+      rx="5.83333"
+      ry="2.91667"
+      stroke="#CED1DC"
+      strokeWidth="1.5"
+      strokeLinejoin="round"
+    />
+    <circle
+      cx="10.0001"
+      cy="5.83333"
+      r="3.33333"
+      stroke="#CED1DC"
+      strokeWidth="1.5"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default UserIcon;
