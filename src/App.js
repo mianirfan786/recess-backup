69cac89caa0cfd760830271e2ed94b1a224822b8
dharmasarswat <dharmasarswat001@gmail.com>
2023-01-18 17:22:08 +0530

    - added theme to the ui, for future useage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { RouterProvider } from "react-router-dom";
+import { ThemeProvider} from "@mui/material/styles";
 import router from "./router";
+import theme from "./utils/theme";
 
 
 function App() {
   return (
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   );
 }
 
