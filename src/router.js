1477efc1e683a74705737b418c19cb31ee60e06d
dharmasarswat <dharmasarswat001@gmail.com>
2023-01-18 17:27:19 +0530

    - combined all the routes

--- src/router.js
+++ src/router.js
@@ -1,10 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
+import ForgotPassword from "./pages/ForgotPassword";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <div>Hello world!</div>,
-    },
-  ]);
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/signin",
+    element: <SignIn />,
+  },
 
-  export default router
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+  {
+    path: "/forgot-password",
+    element: <ForgotPassword />,
+  },
+]);
+
+export default router;
