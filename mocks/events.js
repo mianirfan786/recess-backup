fe916c987cf504ace36a044b4e2b6b06f572c7fc
rakesh <rakeshbishnoi4577@gmail.com>
2023-03-15 09:35:44 +0530

    mockup

--- mocks/events.js
+++ mocks/events.js
@@ -23,16 +23,5 @@ export const event = [
     title: "Title 01",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    category: ["peds", "training", "progress", "nutrition"],
-    type: "event",
-    media: [
-      {
-        link: "https://cdn.discordapp.com/attachments/891933566571798548/1048227794481582130/unknown.png?width=791&height=527",
-      },
-      {
-        link: "https://media.discordapp.net/attachments/891933566571798548/1048228129132523642/unknown.png?width=791&height=527",
-      },
-    ],
-    eventDate: "Thu Dec 15 2022 14:10:17 GMT+0530",
   },
 ];
