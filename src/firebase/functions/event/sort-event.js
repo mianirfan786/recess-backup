1c5346cffa97a3b5d76eb3d1b0f57963989be096
sahil <sahil.bhatiya@catalyst.sh>
2023-03-17 16:15:38 +0530

    Improved Sorting & Added Filtering

--- src/firebase/functions/event/sort-event.js
+++ src/firebase/functions/event/sort-event.js
@@ -1,4 +1,4 @@
-import {collection, getDocs, getFirestore, limit, orderBy, query} from "firebase/firestore";
+import {collection, getDocs, getFirestore, limit, orderBy, query, where} from "firebase/firestore";
 
 import app from "../../config";
 
@@ -23,34 +23,75 @@ export const SortEventByPopular = async (maxItems) => {
 }
 /* sort event By Popularity :: End */
 
-/* sort event by time creation :: Start */
-export const SortEventByTimeStamp = async (maxLimit) => {
-    const q = query(collection(db, "events"), orderBy("timeStamp", "desc"), limit(maxLimit));
-    const querySnapshot = await getDocs(q);
-    return querySnapshot.docs.map((doc) => ({id: doc.id, ...doc.data()}));
-}
-/* sort event by time creation :: End */
 
-/* sort event by max players :: Start */
-export const SortEventByMaxPlayers = async (maxPlayers, maxLimit) => {
-    const q = query(collection(db, "events"), orderBy("maxParticipants", "desc"), limit(maxLimit));
-    const querySnapshot = await getDocs(q);
-    return querySnapshot.docs.map((doc) => ({id: doc.id, ...doc.data()}));
-}
-/* sort event by max players :: End */
+/* -------------------------------------- */
+/* For Event Page */
 
 /* sort event by no. of attendees :: Start */
-export const SortEventByAttendees = async (maxLimit) => {
-    const q = query(collection(db, "events"), orderBy("attendees", "desc"), limit(maxLimit));
+export const SortEventByAttendees = async (maxLimit, minPriceLimit, maxPriceLimit) => {
+    if(minPriceLimit === -1 && maxPriceLimit === -1) {
+        var q = query(collection(db, "events"), orderBy("attendees", "desc"), limit(maxLimit));
+    }else if(minPriceLimit=== 0 && maxPriceLimit === 0){
+        var q = query(collection(db, "events"), where("cost", "==", 0), orderBy("attendees", "desc"), limit(maxLimit));
+    }else{
+        var q = query(collection(db, "events"), where("cost", ">=", minPriceLimit), where("cost", "<=", maxPriceLimit), orderBy("attendees", "desc"), limit(maxLimit));
+    }
     const querySnapshot = await getDocs(q);
     return querySnapshot.docs.map((doc) => ({id: doc.id, ...doc.data()}));
 }
 /* sort event by no. of attendees :: End */
 
 /* sort event by No of participants :: Start */
-export const SortEventByParticipantCount = async ( maxLimit) => {
-    const q = query(collection(db, "events"), orderBy("participantCount", "desc"), limit(maxLimit));
+export const SortEventByParticipantCount = async (maxLimit, minPriceLimit, maxPriceLimit) => {
+    if(minPriceLimit === -1 && maxPriceLimit === -1) {
+        var q = query(collection(db, "events"), orderBy("participantCount", "desc"), limit(maxLimit));
+    }else if(minPriceLimit=== 0 && maxPriceLimit === 0){
+        var q = query(collection(db, "events"), where("cost", "==", 0), orderBy("participantCount", "desc"), limit(maxLimit));
+    }else{
+        var q = query(collection(db, "events"), where("cost", ">=", minPriceLimit), where("cost", "<=", maxPriceLimit), orderBy("participantCount", "desc"), limit(maxLimit));
+    }
     const querySnapshot = await getDocs(q);
     return querySnapshot.docs.map((doc) => ({id: doc.id, ...doc.data()}));
 }
 /* sort event by No of participants :: End */
+
+/* sort event by time creation :: Start */
+export const SortEventByTimeStamp = async (maxLimit, minPriceLimit, maxPriceLimit) => {
+    if(minPriceLimit === -1 && maxPriceLimit === -1) {
+        var q = query(collection(db, "events"), orderBy("timeStamp", "desc"), limit(maxLimit));
+    }else if(minPriceLimit=== 0 && maxPriceLimit === 0){
+        var q = query(collection(db, "events"), where("cost", "==", 0), orderBy("timeStamp", "desc"), limit(maxLimit));
+    }else{
+        var q = query(collection(db, "events"), where("cost", ">=", minPriceLimit), where("cost", "<=", maxPriceLimit), orderBy("timeStamp", "desc"), limit(maxLimit));
+    }
+    const querySnapshot = await getDocs(q);
+    return querySnapshot.docs.map((doc) => ({id: doc.id, ...doc.data()}));
+}
+/* sort event by time creation :: End */
+
+/* sort event by start date :: Start */
+export const SortEventByStartDate = async (maxLimit, minPriceLimit, maxPriceLimit) => {
+    if(minPriceLimit === -1 && maxPriceLimit === -1) {
+        var q = query(collection(db, "events"), orderBy("date", "desc"), limit(maxLimit));
+    }else if(minPriceLimit=== 0 && maxPriceLimit === 0){
+        var q = query(collection(db, "events"), where("cost", "==", 0), orderBy("date", "desc"), limit(maxLimit));
+    }else{
+        var q = query(collection(db, "events"), where("cost", ">=", minPriceLimit), where("cost", "<=", maxPriceLimit), orderBy("date", "desc"), limit(maxLimit));
+    }
+    const querySnapshot = await getDocs(q);
+    return querySnapshot.docs.map((doc) => ({id: doc.id, ...doc.data()}));
+}
+
+/* sort event by max players :: Start */
+export const SortEventByMaxPlayers = async (maxPlayers, maxLimit, minPriceLimit, maxPriceLimit) => {
+    if(minPriceLimit === -1 && maxPriceLimit === -1) {
+        var q = query(collection(db, "events"), orderBy("maxParticipants", "desc"), limit(maxLimit));
+    }else if(minPriceLimit=== 0 && maxPriceLimit === 0){
+        var q = query(collection(db, "events"), where("cost", "==", 0), orderBy("maxParticipants", "desc"), limit(maxLimit));
+    }else{
+        var q = query(collection(db, "events"), where("cost", ">=", minPriceLimit), where("cost", "<=", maxPriceLimit), orderBy("maxParticipants", "desc"), limit(maxLimit));
+    }
+    const querySnapshot = await getDocs(q);
+    return querySnapshot.docs.map((doc) => ({id: doc.id, ...doc.data()}));
+}
+/* sort event by max players :: End */
